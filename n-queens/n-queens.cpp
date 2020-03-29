54376bd16c3876729e58950b57f40031567ef03b
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-03-29 17:57:49 +0200

    Update n-queens.cpp

--- n-queens/n-queens.cpp
+++ n-queens/n-queens.cpp
@@ -4,6 +4,7 @@
 /// @version 1
 /// @date 2018
 /// @brief solve the n-queeen problem with the depth-first search
+///	see : http://www.csplib.org/Problems/prob054/
 ///
 /// @compilation: g++ n-queens.cpp -std=c++0x -Wall -O -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
 ///
